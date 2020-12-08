ad8c7f7b1b8e12f02dafc562f2d58ec1753d2fa7
lirong <lirong@zhixuezhen.com>
2020-12-08 16:52:11 +0800

    发布提交

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-cd  /Users/rongli/workspace/mkdoc/smileMrLee
+cd  /Users/rongli/workspace/github/smilemrlee-source
 mkdocs build
-cp -rf /Users/rongli/workspace/mkdoc/smileMrLee/site/ /Users/rongli/workspace/github/smileMrLee.github.io/ 
+cp -rf /Users/rongli/workspace/github/smilemrlee-source/site/ /Users/rongli/workspace/github/smileMrLee.github.io/ 
 cd /Users/rongli/workspace/github/smileMrLee.github.io
 git add .
 git commit -m "发布提交"
